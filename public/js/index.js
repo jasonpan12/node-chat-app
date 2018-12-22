8a2db39c4f1c9842e8d26ba2d5b252ef35cda770
Jason Pan <jpan@box.com>
2018-12-22 11:06:02 -0600

    emit new message on createmessage

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+// client side javascript
+
+var socket = io();
+
+socket.on('connect', function(){
+  console.log('Connected to server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('newMessage:', message);
+});
+
+socket.on('disconnect', function(){
+  console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function(email) { // email comes from socket.emit
+  console.log('New Email', email);
+});
